f9ab3304adacaa811d25e4b50a6da4cd096fcf08
Fabio Welker <fabiowelker@outlook.com>
2023-04-02 19:41:16 -0300

    Palavra sealed

---
+++ HerancaSealed/Heranca2/Entities/BusinessAccount.cs
@@ -0,0 +1,34 @@
+﻿using Heranca2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca2.Entities
+{
+    class BusinessAccount : Account //Herdar atributos de Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        //metodos
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
