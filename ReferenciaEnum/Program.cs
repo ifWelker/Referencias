e3eccf7a9d650a9aba011747931f8da968d2fe3f
Fabio Welker <fabiowelker@outlook.com>
2023-04-02 16:08:50 -0300

    Primeiro commit

---
+++ ReferenciaEnum/Program.cs
@@ -0,0 +1,29 @@
+﻿using CursoCSharp.Entities;
+using CursoCSharp.Entities.Enuns;
+using System;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order()
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+
+            Console.WriteLine(txt);
+        }
+    }
+}
