2bc55f7269703c89533da31dd0d899277388e513
Fabio Welker <fabiowelker@outlook.com>
2023-04-02 18:58:31 -0300

    Sobreposicao, virtual e base

---
+++ Heranca2.0/Heranca2/Entities/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca2.Entities
+{
+    class Account
+    {
+
+        //Atributos + Construtores
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //Metodos
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}
