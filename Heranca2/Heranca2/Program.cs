401d8099b3e11d8f47c3c3097424a942c883be6c
Fabio Welker <fabiowelker@outlook.com>
2023-04-02 17:18:48 -0300

    UPCASTING E DOWNCASTING

---
+++ Heranca2/Heranca2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Heranca2.Entities;
+
+namespace Heranca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0, 500);
+
+            // UPCASTING - Converte uma SubClasse em uma SuperClasse
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0, 200);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0, 0.01);
+
+            //DOWNCASTING - Converte uma SuperClasse em uma SubClasse
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            //BusinessAccount acc5 = (BusinessAccount)acc3;
+            if (acc3 is BusinessAccount)
+            {
+               // BusinessAccount acc5 = (BusinessAccount)acc3; - Uma das Formas de Fazer o Casting
+               BusinessAccount acc5 = acc3 as BusinessAccount; //Essa é a outra Forma de Fazer o Casting
+                acc5.Loan(200);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
+
+        }
+    }
+}
---
+++ ReferenciaHeranca/ReferenciaHerança/Entities/BusinessAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenciaHeranca.Entities
+{
+    class BusinessAccount : Account //Herdar atributos de Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+            
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) 
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        //metodos
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
