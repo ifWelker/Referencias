1cfda7427370a0dce620553fcfaa46da2bb81fec
Fabio Welker <fabiowelker@outlook.com>
2023-04-03 21:07:14 -0300

    Exercicio Polimorfismo/Heranca

---
+++ ExercicioPropostoPolimorfismo/ExercicioPropostoPolimorfismo/Program.cs
@@ -0,0 +1,50 @@
+﻿using ExercicioPropostoPolimorfismo.Entities;
+using System;
+using System.Globalization;
+
+namespace ExercicioPropostoPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char ch =char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                if (ch == 'c')
+                {
+                    list.Add(new Product(name, price));
+                }
+                else if (ch == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product prod in list)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+        }
+    }
+}
