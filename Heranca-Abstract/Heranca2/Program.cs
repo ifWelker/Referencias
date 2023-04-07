b489873716126c1e050df618558dc6ae18c4058f
Fabio Welker <fabiowelker@outlook.com>
2023-04-07 11:10:04 -0300

    Abstract

---
+++ Heranca-Abstract/Heranca2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Heranca2.Entities;
+using System.Globalization;
+
+namespace Heranca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> list = new List<Account>();
+
+            list.Add(new SavingsAccount(1001, "Alex", 500, 0.01));
+            list.Add(new BusinessAccount(1002, "Maria", 500, 400));
+            list.Add(new SavingsAccount(1003, "Bob", 500, 0.01));
+            list.Add(new BusinessAccount(1004, "Ana", 500, 500));
+
+            double sum = 0;
+            foreach (Account account in list) 
+            {
+                sum += account.Balance;
+            }
+
+            Console.WriteLine("Total Balance: " + sum.ToString("F2", CultureInfo.InvariantCulture));
+            foreach (Account acc in list)
+            {
+                acc.Withdraw(10.0);
+            }
+
+            foreach (Account acc in list)
+            {
+                Console.WriteLine("Updated balance for account "
+                    + acc.Number
+                    + ": "
+                    + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
