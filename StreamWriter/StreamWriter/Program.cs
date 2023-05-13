9dd04f914ee7b7c117190c0cc4a60019bdf278c5
ifWelker <fabiowelker@outlook.com>
2023-05-12 22:30:37 -0300

    RefStreamWriter

---
+++ StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ReferenciaStreamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\temp\file1.txt";
+            string targetPath = @"C:\temp\file2.txt";
+
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("An Error Ocurried");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
