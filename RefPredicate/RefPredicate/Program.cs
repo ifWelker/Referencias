834c9599f5dd51ebe4135ba1c1ac6f63f417bc5c
ifWelker <fabiowelker@outlook.com>
2023-05-12 22:29:38 -0300

    RefPredicate

---
+++ RefPredicate/RefPredicate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RefPredicate.Entities;
+
+//Fazer um Programa que, a partir de uma lista de produtos, remova da lista somente aqueles cujo preço minimo seja 100.
+
+namespace RefPredicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(ProductTest);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p) 
+        {
+            return p.Price >= 100;
+        }
+    }
+}
